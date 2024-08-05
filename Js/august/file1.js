5bc18c1c9c6d68d49c70a0b9ae3eb09a7734c137
jatin thakur <97386157+jatinthakur9@users.noreply.github.com>
2024-08-05 15:36:48 +0530

    5 aug

--- Js/august/file1.js
+++ Js/august/file1.js
@@ -1,80 +1,224 @@
 
-// declare a variable
-var firstname = "jatin thakur";
-// use a variable
-console.log(firstname);
+// // declare a variable
+// var firstname = "jatin thakur";
+// // use a variable
+// console.log(firstname);
 
-// change the variable
-firstname = " l king"
+// // change the variable
+// firstname = " l king"
 
-console.log(firstname);
+// console.log(firstname);
 
- var para = "hello my name is jatin           ";
+//  var para = "hello my name is jatin           ";
 
- console.log(para[8]);
-
-
-//  string 
-// numbers
-// boolens
-// undfined
-//null
-// bigint
-// symbol
-
-// conversion of number to string and string to number
-let age = 24;
-let namee = "l for king";
-
-let ans = true;
-
-console.log(typeof ans)
-
-let stringage = age +" ";
+//  console.log(para[8]);
 
 
-console.log(stringage)
+// //  string 
+// // numbers
+// // boolens
+// // undfined
+// //null
+// // bigint
+// // symbol
+
+// // conversion of number to string and string to number
+// let age = 24;
+// let namee = "l for king";
+
+// let ans = true;
+
+// console.log(typeof ans)
+
+// let stringage = age +" ";
 
 
- let numname = +namee;
-
- console.log(typeof numname)
- console.log( Number(namee))
+// console.log(stringage)
 
 
-// string concetination
+//  let numname = +namee;
+
+//  console.log(typeof numname)
+//  console.log( Number(namee))
 
 
-// bigint
- let car = 453453453453453453454875309485734985n;
-  console.log();
-
-//   ternary operator or conditional operator
-
-let agee = 6;
-
-let drink = age<=3 ? 'water' : " gangajal";
+// // string concetination
 
 
-console.log(drink);
+// // bigint
+//  let car = 453453453453453453454875309485734985n;
+//   console.log();
 
-// while loop example 
+// //   ternary operator or conditional operator
 
-let number = 10 ;
- let sum = 0;
-  let i = 0;
+// let agee = 6;
 
-while(i<=number){
-    console.log(i);
-    sum +=i;
-  i++;
-}
-
-console.log(sum);
+// let drink = age<=3 ? 'water' : " gangajal";
 
 
+// console.log(drink);
 
-// arrays 
+// // while loop example 
+
+// let number = 10 ;
+//  let sum = 0;
+//   let i = 0;
+
+// while(i<=number){
+//     console.log(i);
+//     sum +=i;
+//   i++;
+// }
+
+// console.log(sum);
+
+
+
+// // arrays
+// // create an array
+
+// let nms = ["jatin" , "sarthak" , "himanshu " , "David"];
+
+// console.log(nms);
+// let mix = [1 , 1.4 , "jatin" , null , true];
+//   mix[1] ="jatinm";
+// console.log(mix);
+
+
+// // push pop  unshift shift
+// // push adds at last in array
+// mix.push("hell");
+
+// console.log(mix);
+
+
+
+// //  pop removes element from last 
+// mix.pop();
+
+// console.log(mix);
+
+// // unshift adds element at first
+// mix.unshift("drugs");
+// console.log(mix);
+
+
+// // shift removes element from starting
+
+// mix.shift();
+// console.log(mix);
+
+// // push pop are faster then shift unshift
+
+
+
+// // primitve and refrence data types
+
+// // let num1 = 6;
+// //  let num2 = num1;
+
+// //  console.log( "First one  " +  num1);
+// //  console.log( "Second one  " +  num2);
+// //  num1++;
+
+// //  console.log( "after changing values " );
+
+// //  console.log( "First one  " +  num1);
+// //  console.log( "Second one  " +  num2);
+
+// //  this happen due to primitive data types
+
+
+// // now example of refrence data types
+
+//   // let arr1 = [1 ,2 ,3 ,4 ];
+
+//   // let arr2 = arr1;
+//   // console.log( "First one  " +  arr1);
+//   // console.log( "Second one  " +  arr2);
+
+//   // arr1.push(4);
+//   // console.log( "after changing values " );
+
+//   // console.log( "First one  " +  arr1);
+//   // console.log( "Second one  " +  arr2);
+
+//   // both changes 
+
+//   // arr2.push(434);
+//   // console.log( "First one  " +  arr1);
+//   // console.log( "Second one  " +  arr2);
+
+// // due to stack and heap concept
+
+
+// // clone an array
+
+// // with slice method 
+
+// let array1 = [2,23,4,3];
+//  let array2 = array1.slice(0);
+ 
+//  console.log( "First one  " +  array1);
+//  console.log( "Second one  " +  array2);
+
+//  array2.push('jatin');
+
+//  console.log( "First one  " +  array1);
+//  console.log( "Second one  " +  array2); 
+
+
+//  with .concat
+
+// let array1 = [2,23,4,3];
+//  let array2 = [].concat(array1);
+ 
+//  console.log( "First oneee  " +  array1);
+//  console.log( "Second one  " +  array2);
+
+//  array2.push('jatin');
+
+//  console.log( "First one  " +  array1);
+//  console.log( "Second one  " +  array2); 
+
+
+//  with spread oprator
+
+// let array1 = [2,23,4,3];
+//  let array2 = [...array1];
+  
+//  console.log( "First oneeee  " +  array1);
+//  console.log( "Second one  " +  array2);
+
+//  array2.push('jatin');
+
+//  console.log( "First one  " +  array1);
+//  console.log( "Second one  " +  array2); 
+
+
+// let array1 = [2,23,4,3];
+//  let array2 = [...array1].concat(["india" , "pakistan"]);
+  
+//  console.log( "First oneeee  " +  array1);
+//  console.log( "Second one  " +  array2);
+
+//  array2.push('jatin');
+
+//  console.log( "First one  " +  array1);
+//  console.log( "Second one  " +  array2); 
+
+
+
+// for loop in array
+
+
+
+
+ 
+
+
+
+
 
 
 
