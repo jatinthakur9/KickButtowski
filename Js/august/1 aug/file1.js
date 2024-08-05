d5aad46cb182db39fbad47f2b813d2af5d630c5f
jatin thakur <97386157+jatinthakur9@users.noreply.github.com>
2024-08-05 14:05:20 +0530

    1 aug

---
+++ Js/august/1 aug/file1.js
@@ -0,0 +1,80 @@
+
+// declare a variable
+var firstname = "jatin thakur";
+// use a variable
+console.log(firstname);
+
+// change the variable
+firstname = " l king"
+
+console.log(firstname);
+
+ var para = "hello my name is jatin           ";
+
+ console.log(para[8]);
+
+
+//  string 
+// numbers
+// boolens
+// undfined
+//null
+// bigint
+// symbol
+
+// conversion of number to string and string to number
+let age = 24;
+let namee = "l for king";
+
+let ans = true;
+
+console.log(typeof ans)
+
+let stringage = age +" ";
+
+
+console.log(stringage)
+
+
+ let numname = +namee;
+
+ console.log(typeof numname)
+ console.log( Number(namee))
+
+
+// string concetination
+
+
+// bigint
+ let car = 453453453453453453454875309485734985n;
+  console.log();
+
+//   ternary operator or conditional operator
+
+let agee = 6;
+
+let drink = age<=3 ? 'water' : " gangajal";
+
+
+console.log(drink);
+
+// while loop example 
+
+let number = 10 ;
+ let sum = 0;
+  let i = 0;
+
+while(i<=number){
+    console.log(i);
+    sum +=i;
+  i++;
+}
+
+console.log(sum);
+
+
+
+
+
+
+
