ff19ab5925145ccc4cb0cf8d2d9d55eddc3f9ab8
jatin thakur <97386157+jatinthakur9@users.noreply.github.com>
2024-09-03 08:47:47 +0530

    array methods completed

--- Js/1.js
+++ Js/1.js
@@ -186,4 +186,155 @@
 // console.log(arrobj);
 
 
+//find method 
 
+// const pets  = [ "gdf" ,"gof","dog ","gof" ]
+
+
+// function l3(string){
+//     return string.length ==3;
+
+// }
+
+
+// const j = pets.find(l3);
+
+// console.log(j);
+
+// // in array with obj find method
+
+// const arrobj = [
+
+// {firstname:"jatin"  , age: 22  , uiid:5951},
+// {firstname:"j"  , age: 1, uiid:5952    },
+// {firstname:"jat"  , age: 252   , uiid:5953 },
+// {firstname:"jain"  , age: 282    , uiid:5954 },
+
+
+
+// ];
+
+
+// function id(u){
+//   return u.uiid==5951;
+// } 
+
+
+
+// const s = arrobj.find(id);
+
+// console.log(s);
+
+
+//every method 
+
+// const num = [2,6,8,5,10];
+
+
+// function ideven(number){
+
+//     return number%2==0;
+
+
+// }
+
+
+
+// const ans = num.every(ideven);
+
+// console.log(ans);
+
+
+// every method with array obj
+
+
+// const arrobj = [
+
+// {firstname:"jatin"  , age: 22  , uiid:5951},
+// {firstname:"j"  , age: 1, uiid:5952    },
+// {firstname:"jat"  , age: 252   , uiid:5953 },
+// {firstname:"jain"  , age: 282    , uiid:5954 },
+
+
+
+// ];
+
+
+// function idcheck(id){
+//     return id.uiid>=5951;
+// }
+
+
+
+// const ans = arrobj.every(idcheck);
+
+// console.log(ans);
+
+
+// some  method  
+
+
+// if even single conditon is matched it will return true
+
+// const num = [2,6,8,8,10];
+
+
+// function ideven(number){
+
+//     return number%2!==0;
+
+
+// }
+
+
+
+// const ans = num.some(ideven);
+
+// console.log(ans);
+
+
+// some method with array obj
+
+
+// const arrobj = [
+
+// {firstname:"jatin"  , age: 22  , uiid:5951},
+// {firstname:"j"  , age: 1, uiid:5952    },
+// {firstname:"jat"  , age: 252   , uiid:5953 },
+// {firstname:"jain"  , age: 282    , uiid:5954 },
+
+
+
+// ];
+
+
+// function idcheck(id){
+//     return id.uiid<5951;
+// }
+
+
+
+// const anss = arrobj.some(idcheck);
+
+// console.log(anss);
+
+
+// fill method
+ 
+
+
+// const arr = new Array(10).fill(5 , 0 , 10);
+
+// console.log(arr)
+
+
+//splice method 
+// start  , delete  , insert
+
+
+const item = ["item1","item2","item3","item3"];
+
+
+item.splice(1, 2 , "jatin" ,"is great");
+
+console.log(item);
